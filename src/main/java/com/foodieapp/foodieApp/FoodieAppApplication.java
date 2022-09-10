900a741eb113ec0d177a0b79648b700f1d9e551d
Deepak S <sdeepak101@gmail.com>
2022-09-10 11:01:45 +0530

    First commit

---
+++ src/main/java/com/foodieapp/foodieApp/FoodieAppApplication.java
@@ -0,0 +1,14 @@
+package com.foodieapp.foodieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieAppApplication.class, args);
+		System.out.println("Hello Test APP");
+	}
+
+}
